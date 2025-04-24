3d8b2801d9f949ad16936a8ac62fa894f56107a9
Edgar Sousa <stratulatedgar@gmail.com>
2025-04-24 17:55:11 +0100

    feat: Added route /properties/listing to see all properties

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Properties\PropertyController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('properties')->group(function () {
+    Route::get('listing', [PropertyController::class, 'index'])->name('properties.index');
 });
 
 require __DIR__.'/auth.php';
